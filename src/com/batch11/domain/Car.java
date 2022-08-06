a9a5583020295e7063654265cca447112cb4d197
shahzad.iqbal <shahzad.iqbal@bookassist.com>
2022-08-06 17:28:35 +0500

    new file add car

---
+++ src/com/batch11/domain/Car.java
@@ -0,0 +1,17 @@
+package com.batch11.domain;
+
+public class Car {
+private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
