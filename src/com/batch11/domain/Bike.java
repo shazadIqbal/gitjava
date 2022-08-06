0aac89917789f1d9cc01fb468b73a91e6cfb717b
abdulsami154 <abdulsami156@hotmail.com>
2022-08-06 17:40:59 +0500

    Bike class added

---
+++ src/com/batch11/domain/Bike.java
@@ -0,0 +1,27 @@
+package com.batch11.domain;
+
+public class Bike {
+    private  String name;
+    private Integer model;
+
+    public Bike(String name, Integer model) {
+        this.name = name;
+        this.model = model;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getModel() {
+        return model;
+    }
+
+    public void setModel(Integer model) {
+        this.model = model;
+    }
+}
